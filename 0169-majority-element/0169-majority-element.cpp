77a2400d7e7f7cc69d4e10bc325a65703c9ce9bf
Sumit Kumar <145241353+sumit-solankii@users.noreply.github.com>
2026-07-27 20:23:06 +0530

    Time: 0 ms (100%), Space: 28.2 MB (70.87%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -1,20 +1,25 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-        sort(nums.begin(), nums.end());
-        int n = nums.size();
-        for(int val : nums){
-            int freq = 0;
-            for( int el : nums){
-                if(el == val){
-                    freq++;
-                }
-            }
-            if(freq > n / 2){
-                return val;
-            }
-        }
-        return -1;
+        
+        // METHOD 1
+        // int n = nums.size();
+        // for(int val : nums){
+        //     int freq = 0;
+        //     for( int el : nums){
+        //         if(el == val){
+        //             freq++;
+        //         }
+        //     }
+        //     if(freq > n / 2){
+        //         return val;
+        //     }
+        // }
+        // return -1;
+
+
+        // METHOD 2
+        // sort(nums.begin(), nums.end());
         // int freq = 1;
 
         // int ans = nums[0];
@@ -33,18 +38,20 @@ public:
         // }
         
         // return ans;
-        // int freq = 0;
-        // int ans = 0;
-        // for( int i = 0; i < nums.size(); i++ ){
-        //     if(freq == 0){
-        //         ans = nums[i];
-        //     }
-        //     if(ans == nums[i]){
-        //         freq++;
-        //     }else{
-        //         freq--;
-        //     }
-        // }
-        // return ans;
+
+        // METHOD 3
+        int freq = 0;
+        int ans = 0;
+        for( int i = 0; i < nums.size(); i++ ){
+            if(freq == 0){
+                ans = nums[i];
+            }
+            if(ans == nums[i]){
+                freq++;
+            }else{
+                freq--;
+            }
+        }
+        return ans;
     }
 };
